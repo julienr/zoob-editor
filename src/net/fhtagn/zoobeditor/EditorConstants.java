4819fda9031dfebc917df180087803ddfb61c269
Julien Rebetez <julien@fhtagn.net>
2010-07-27 23:30:11 +0200

    Google login support using mytracks code

---
+++ src/net/fhtagn/zoobeditor/EditorConstants.java
@@ -0,0 +1,13 @@
+package net.fhtagn.zoobeditor;
+
+public class EditorConstants {
+	public static final String TAG  = "ZoobEditor";
+	
+	public static final String ACCOUNT_TYPE = "com.google";
+	
+	/**
+	 * onActivityResult request code
+	 */
+	public static final int GET_LOGIN = 1;
+	public static final int SEND_TO_ZOOB_WEB = 2;
+}
--- src/net/fhtagn/zoobeditor/browser/OnlineSeriesActivity.java
+++ src/net/fhtagn/zoobeditor/browser/OnlineSeriesActivity.java
@@ -1,15 +1,27 @@
 package net.fhtagn.zoobeditor.browser;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class OnlineSeriesActivity extends Activity {
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		TextView textView = new TextView(this);
-		textView.setText("Online levels");
-		setContentView(textView);
+		Button loginButton = new Button(this);
+		loginButton.setText("Login");
+		loginButton.setOnClickListener(new OnClickListener() {
+			@Override
+      public void onClick(View arg0) {
+				Intent i = new Intent(getApplicationContext(), AccountList.class);
+				startActivity(i); 
+      }
+			
+		});
+		setContentView(loginButton);
 	}
 }
