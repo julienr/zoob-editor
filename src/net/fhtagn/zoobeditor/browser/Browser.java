7e73b7ba4a0b403d72bf60c04f16c2f24d296d60
Julien Rebetez <julien@fhtagn.net>
2010-07-30 16:08:07 +0200

    Better-looking tabs

--- src/net/fhtagn/zoobeditor/browser/Browser.java
+++ src/net/fhtagn/zoobeditor/browser/Browser.java
@@ -1,12 +1,17 @@
 package net.fhtagn.zoobeditor.browser;
 
 import net.fhtagn.zoobeditor.R;
+import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class Browser extends TabActivity {
 	static final String TAG = "ZoobEditor";
@@ -14,7 +19,6 @@ public class Browser extends TabActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 		
 		Resources res = getResources();
 		TabHost tabHost = getTabHost();
@@ -23,28 +27,26 @@ public class Browser extends TabActivity {
 		
 		//My levels
 		intent = new Intent().setClass(this, MySeriesActivity.class);
-		spec = tabHost.newTabSpec("myseries").setIndicator(res.getString(R.string.myseries_tab))
+		spec = tabHost.newTabSpec("myseries").setIndicator(res.getString(R.string.myseries_tab)/*, res.getDrawable(android.R.drawable.ic_menu_myplaces)*/)
 								  .setContent(intent);
 		tabHost.addTab(spec);
 		
 		//Online levels
 		intent = new Intent().setClass(this, OnlineSeriesActivity.class);
-		spec = tabHost.newTabSpec("online").setIndicator(res.getString(R.string.online_tab))
+		spec = tabHost.newTabSpec("online").setIndicator(res.getString(R.string.online_tab)/*, res.getDrawable(android.R.drawable.ic_menu_mapmode)*/)
 									.setContent(intent);
 		tabHost.addTab(spec);
 		
 		tabHost.setCurrentTab(0);
 		
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); 
-	}
-
-	
-	/*@Override
-	public void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		instance = this;
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		
-		showDialog(DIALOG_PROGRESS);
-		authenticate(new Intent(), EditorConstants.SEND_TO_ZOOB_WEB);
-	}*/
+		//FIXME: big hack, but found no other way to achieve that.
+		//Since we don't us icons in our tabs, reduce tab height. 
+		TabWidget tw = getTabHost().getTabWidget();
+		for (int i=0; i<tw.getChildCount(); i++) {
+			final float scale = res.getDisplayMetrics().density;
+			tw.getChildAt(i).getLayoutParams().height = (int)(scale*40); //40 dip for tab height (converted to pixels by using scale 
+		}
+	}
 }
